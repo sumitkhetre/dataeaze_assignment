f4013880199cf21e99c12473edc33b5834aba6eb
sumit khetre <74355076+sumitkhetre@users.noreply.github.com>
2021-03-06 09:29:33 +0530

    script

---
+++ execution.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ' ---------------------------Question 1----------------------------------------'
+spark-submit  /home/sunbeam/dataeaze/Question1.py
+echo '--------------------------- Question 2----------------------------------------'
+spark-submit  /home/sunbeam/dataeaze/Question2.py
+echo '--------------------------- Question 3-----------------------------------------'
+spark-submit  /home/sunbeam/dataeaze/Question3.py
+echo ' ----------------------------Question 4------------------------------------------'
+spark-submit  /home/sunbeam/dataeaze/Question4.py
+echo ' ----------------------------ALL_ANSWERS-------------------------------------'
+spark-submit  /home/sunbeam/dataeaze/ALL_ANSWERS.py
+
+echo '---------------------------------end script-------------------------------------------'
+
+
